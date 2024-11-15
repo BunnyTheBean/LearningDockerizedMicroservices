1b8e24fbdbf8de99b61d82a3f595bb8a1fd612ef
Harzendorf, Nico <Nico.Harzendorf@lecos.de>
2024-11-15 13:20:37 +0100

    wipfeat: add initial book form and table components

--- BookArchive/Frontend/BookArchive/src/app/app.component.ts
+++ BookArchive/Frontend/BookArchive/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BookTableComponent } from './components/book-table/book-table.component';
+import { BookFormComponent } from './components/book-form/book-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet, BookTableComponent, BookFormComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
